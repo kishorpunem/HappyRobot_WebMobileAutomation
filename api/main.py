06206cb20b9b64d4b0da16b39d6c8430b0181d9c
PunemKishor001 <punemkishor01@gmail.com>
2026-04-01 22:11:10 +0530

    FrontEnd Code CHnageS

--- api/main.py
+++ api/main.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 from api.routes.automation_routes import router as automation_router
 from api.routes.health_routes import router as health_router
 
 app = FastAPI(title="HappyRobot Automation API")
 
+# Templates folder
+templates = Jinja2Templates(directory="templates")
+
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow frontend requests
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,6 +23,10 @@ app.add_middleware(
 app.include_router(health_router)
 app.include_router(automation_router)
 
-@app.get("/")
-def root():
-    return {"message": "HappyRobot Automation API is running"}
+# Dashboard UI
+@app.get("/", response_class=HTMLResponse)
+async def dashboard(request: Request):
+    return templates.TemplateResponse(
+        "dashboard.html",
+        {"request": request}
+    )
